903fbbc8dc7dbe5cd3a37164b05e181a8cc6a2ac
julianmartire1 <julianmartire.jm@gmail.com>
2019-12-19 17:42:18 -0300

    url

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,9 @@
 process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlD;
-/* if(process.env.NODE_ENV === 'dev') {
+if(process.env.NODE_ENV === 'dev') {
     urlD = 'mongodb://localhost:27017/cafe';
-} else { */
+} else {
     urlD = 'mongodb://julianmartire1:hayequipo123456@ds263436.mlab.com:63436/hay-equipo';
-/* } */
+}
 process.env.URLDB = urlD;
